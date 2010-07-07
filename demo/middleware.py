c3b5fe6aa91e8fd802c1f892ef3f32e672cc3f28
stephenmcd <none@none>
2010-07-07 10:33:38 +1000

    Added middleware preventing demo user from changing their password.

---
+++ demo/middleware.py
@@ -0,0 +1,17 @@
+
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+
+
+class BlockPasswordChange(object):
+
+    def process_request(self, request):
+        """
+        Prevent the demo user from changing their password.
+        """
+        if (request.path == reverse("admin:password_change") and    
+            request.user.is_authenticated() and 
+            request.user.username == "demo"):
+            return HttpResponse("<h1>Unavailable</h1>"
+                "<p>This featured is disabled for the demo account.</p>")
+        
