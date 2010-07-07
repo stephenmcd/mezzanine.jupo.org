2611596e0d184dc60390fdc81c49319959e6295a
stephenmcd <none@none>
2010-07-07 13:32:41 +1000

    Redo the reset_demo command to not use reset_db since that doesn't support Django 1.2 or Postgres.

--- demo/management/commands/reset_demo.py
+++ demo/management/commands/reset_demo.py
@@ -1,4 +1,5 @@
 
+from django.db.models import get_models
 from django.contrib.auth.models import User, Permission
 from django.contrib.sites.models import Site
 from django.core.management import call_command
@@ -12,23 +13,25 @@ class Command(NoArgsCommand):
     """
     Resets the entire Mezzanine demo site on a scheduled basis, steps are:
     
-    1) Destroy the database.
-    2) Rebuild the database.
-    3) Create the demo user.
-    4) Give the demo user permissions to the Mezzanine apps.
-    5) Import a Tumblr blog.
-    6) Set the current site domain.
+    1) Empty all models.
+    2) Reload fixutres.
+    3) Set the current site.
+    4) Create the demo user.
+    5) Give the demo user permissions to the Mezzanine apps.
+    6) Import a Tumblr blog.
     """
 
     def handle_noargs(self, **options):
         options["interactive"] = False
-        call_command("reset_db", **options)
+        for model in get_models():
+            model.objects.all().delete()
         call_command("syncdb", **options)
+        Site.objects.create(domain="mezzanine.jupo.org", 
+            name="Mezzanine Demo")
         user = User.objects.create_user("demo", "example@example.com", "demo")
         user.is_staff = True
         for perm in Permission.objects.filter(content_type__name__in=perms):
             user.user_permissions.add(perm)
             user.save()
         call_command("import_tumblr", "steve-mc", "demo", **options)
-        Site.objects.all().update(domain="mezzanine.jupo.org", 
-            name="Mezzanine Demo")
+
