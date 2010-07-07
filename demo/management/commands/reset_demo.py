9305275e4a9ef0b0596be4b49bffd5a9e5374d90
stephenmcd <none@none>
2010-07-07 15:00:22 +1000

    Greatly reduce the scope of the reset_demo command since earlier approaches break certain edge cases. It now operates on models for mezzanine apps only and puts the onus ono having fixtures installed.

--- demo/management/commands/reset_demo.py
+++ demo/management/commands/reset_demo.py
@@ -4,38 +4,24 @@ from shutil import rmtree
 
 from django.db.models import get_models
 from django.conf import settings
-from django.contrib.auth.models import User, Permission
 from django.core.management import call_command
 from django.core.management.base import NoArgsCommand
 
 
-perms = ["Blog post", "Page", "Comment"]
-
-
 class Command(NoArgsCommand):
     """
-    Resets the entire Mezzanine demo site on a scheduled basis, steps are:
-    
-    1) Empty all models.
-    2) Reload fixutres.
-    3) Create the demo user.
-    4) Give the demo user permissions to the Mezzanine apps.
-    5) Import a Tumblr blog.
-    6) Remove uploaded files.
+    Resets the Mezzanine models for the Mezzanine demo site.
     """
 
     def handle_noargs(self, **options):
-        options["interactive"] = False
+        reset_apps = [a.split(".")[-1] for a in settings.INSTALLED_APPS if 
+            a.startswith("mezzanine.")]
         for model in get_models():
-            if model._meta.object_name != "Site":
+            meta = model._meta
+            if meta.app_label in reset_apps:
+                print "Flushing %s.%s" % (meta.app_label, meta.object_name)
                 model.objects.all().delete()
-        call_command("syncdb", **options)
-        user = User.objects.create_user("demo", "example@example.com", "demo")
-        user.is_staff = True
-        for perm in Permission.objects.filter(content_type__name__in=perms):
-            user.user_permissions.add(perm)
-            user.save()
-        call_command("import_tumblr", "steve-mc", "demo", **options)
+        call_command("loaddata", "initial_data", **options)
         uploads = os.path.join(settings.MEDIA_ROOT, "uploads")
         rmtree(uploads)
         os.mkdir(uploads)
