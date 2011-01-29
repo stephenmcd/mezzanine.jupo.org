62187459ea508c0292f70718a02f5700b462a998
stephenmcd <steve@jupo.org>
2011-01-29 18:40:29 +1100

    Added rotating quotes to the homepage.

--- urls.py
+++ urls.py
@@ -5,9 +5,13 @@ from django.views.generic.simple import direct_to_template
 from mezzanine import __version__
 from mezzanine.project_template.urls import urlpatterns
 
+from demo.utils import quotes
+
+home_dict = {"template": "index.html", "extra_context": 
+            {"version": __version__, "quotes": quotes()}}
+
 urlpatterns = patterns("",
-    url("^$", direct_to_template, {"template": "index.html", 
-        "extra_context": {"version": __version__}}, name="home"),
+    url("^$", direct_to_template, home_dict, name="home"),
     ("^shop/", include("cartridge.shop.urls")),
 ) + urlpatterns
 
