d3b694be3b4089cd36d317fe4ba1d959a46b31cb
stephenmcd <steve@jupo.org>
2011-08-13 11:17:32 +1000

    Added sites gallery

--- demo/utils.py
+++ demo/utils.py
@@ -8,26 +8,34 @@ import mezzanine
 from mezzanine.utils.importing import path_for_import
 
 
-def quotes():
+def readme():
     """
-    Pulls the list of quotes about Mezzanine from the README file 
-    and converts it from RST to HTML. Will only exist if Mezzanine 
-    is installed from the repo, which is the case for the demo site.
+    Converts the README file from RST to HTML.
     """
-
     writer = Writer()
     writer.translator_class = HTMLTranslator
     readme = os.path.join(path_for_import("mezzanine"), "..", "README.rst")
-
     try:
         with open(readme, "r") as f:
             data = f.read()
     except IOError:
         return ""
-    else:
-        data = publish_string(data, writer=writer)
-        data = data.split("<h1>Quotes</h1>")[1].split("</div>")[0]
-        data = data.replace(" - <a", "<br /> - <a")
-        data = data.replace("<li", "<li class=\"first\"", 1)
-        return data
+    return publish_string(data, writer=writer)
 
+def quotes():
+    """
+    Pulls the list of quotes about Mezzanine from the README file.
+    """
+    html = readme()
+    html = html.split("<h1>Quotes</h1>")[1].split("</div>")[0]
+    html = html.replace(" - <a", "<br /> - <a")
+    html = html.replace("<li", "<li class=\"first\"", 1)
+    return html
+
+def sites():
+    """
+    Pulls the list of sites using Mezzanine from the README file.
+    """
+    html = readme()
+    html = html.split("<h1>Sites Using Mezzanine</h1>")[1].split("<blockquote>")[1].split("</blockquote>")[0]
+    return html
--- urls.py
+++ urls.py
@@ -5,13 +5,15 @@ from django.views.generic.simple import direct_to_template
 from mezzanine import __version__
 from mezzanine.project_template.urls import urlpatterns
 
-from demo.utils import quotes
+from demo.utils import sites, quotes
 
-home_dict = {"template": "index.html", "extra_context": 
+home_dict = {"template": "index.html", "extra_context":
             {"version": __version__, "quotes": quotes()}}
+
+sites_dict = {"template": "sites.html", "extra_context": {"sites": sites()}}
 
 urlpatterns = patterns("",
     url("^$", direct_to_template, home_dict, name="home"),
+    url("^sites/$", direct_to_template, sites_dict, name="sites"),
     ("^shop/", include("cartridge.shop.urls")),
 ) + urlpatterns
-
