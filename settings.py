6ce8ec05a19a9c7196770707e5104739f683b9be
Stephen McDonald <steve@jupo.org>
2012-01-28 22:40:30 +1100

    update settings

--- settings.py
+++ settings.py
@@ -34,23 +34,68 @@ DATABASES = {
     }
 }
 
-# Paths.
 import os
+
+# Full filesystem path to the project.
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Name of the directory for the project.
 PROJECT_DIRNAME = PROJECT_ROOT.split(os.sep)[-1]
-ADMIN_MEDIA_PREFIX = "/media/"
+
+# Every cache key will get prefixed with this value - here we set it to
+# the name of the directory the project is in to try and use something
+# project specific.
 CACHE_MIDDLEWARE_KEY_PREFIX = PROJECT_DIRNAME
-MEDIA_URL = "/site_media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, MEDIA_URL.strip("/"))
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = "/static/"
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = STATIC_URL + "media/"
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, *MEDIA_URL.strip("/").split("/"))
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
+
+# Package/module name to import the root urlpatterns from for the project.
 ROOT_URLCONF = "%s.urls" % PROJECT_DIRNAME
+
+# Put strings here, like "/home/html/django_templates"
+# or "C:/www/django/templates".
+# Always use forward slashes, even on Windows.
+# Don't forget to use absolute paths, not relative paths.
 TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
 
-# Apps/
+LOGIN_URL = "/shop/account/"
+
+
+################
+# APPLICATIONS #
+################
+
+# django.contrib.sites should be before cartridge.shop to avoid a
+# foreign key error when installing the test fixtures with Postgres,
+# and cartridge.shop should be before mezzanine.core so that the
+# shop's base template is loaded over mezzanine's.
+
 INSTALLED_APPS = list(INSTALLED_APPS)
 INSTALLED_APPS.remove("django.contrib.sites")
 INSTALLED_APPS = tuple(INSTALLED_APPS)
-INSTALLED_APPS = ("demo", "mezzanine.mobile", "django.contrib.sites",
-                  "cartridge.shop") + INSTALLED_APPS
+INSTALLED_APPS = ("django.contrib.sites", "cartridge.shop") + INSTALLED_APPS
 
 TEMPLATE_CONTEXT_PROCESSORS += (
     "cartridge.shop.context_processors.shop_globals",
