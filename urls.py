751845d2ff0057ada0b60537f32933b03af75380
Stephen McDonald <steve@jupo.org>
2012-03-03 23:31:05 +1100

    Launch the new site!

--- urls.py
+++ urls.py
@@ -1,18 +1,16 @@
 
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
+from django.shortcuts import redirect
 
 from mezzanine.project_template.urls import urlpatterns
 
 from demo import home_context
 
 home_dict = {"template": "index.html", "extra_context": home_context}
-new_dict = dict(home_dict, template="new.html")
-sites_dict = dict(home_dict, template="sites.html")
 
 urlpatterns = patterns("",
     url("^$", direct_to_template, home_dict, name="home"),
-    url("^new/$", direct_to_template, new_dict, name="new"),
-    url("^sites/$", direct_to_template, sites_dict, name="sites"),
+    url("^sites/$", lambda r: redirect("/"), name="sites"),
     ("^shop/", include("cartridge.shop.urls")),
 ) + urlpatterns
