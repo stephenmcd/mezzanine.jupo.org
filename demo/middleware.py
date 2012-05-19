c41e1d34690dedeb48fa4c7b0d487a9586afdff1
Stephen McDonald <steve@jupo.org>
2012-05-19 19:53:54 +1000

    Disable demo username change in profile form.

--- demo/middleware.py
+++ demo/middleware.py
@@ -9,9 +9,10 @@ class BlockPasswordChange(object):
         """
         Prevent the demo user from changing their password.
         """
-        password_posted = (request.POST.get("password1") or
-                           request.POST.get("new_password1"))
-        if password_posted and request.user.username == "demo":
-            message = "Password change disabled for the demo account."
-            return HttpResponse("<h1>Unavailable</h1><p>%s</p>" % message)
+        disallowed = (request.POST.get("password1") or
+                      request.POST.get("new_password1") or
+                      request.POST.get("username", "demo") != "demo")
+        if request.user.username == "demo" and disallowed:
+            msg = "Username/password change disabled for the demo account."
+            return HttpResponse("<h1>Unavailable</h1><p>%s</p>" % msg)
 
