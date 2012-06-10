24a3b2dd9da93bc1460b5abe62da7d0e5153245f
Stephen McDonald <steve@jupo.org>
2012-06-10 13:13:21 +1000

    Fix ga code again.

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -36,16 +36,6 @@ $(function() {
         $(this).carousel('next');
     }).carousel();
 
-    // Google analytics
-    _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-52596-12']);
-    _gaq.push(['_trackPageview']);
-    (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-
     // Flattr button
     (function() {
         var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
@@ -57,3 +47,12 @@ $(function() {
 
 });
 
+// Google analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-52596-12']);
+_gaq.push(['_trackPageview']);
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
