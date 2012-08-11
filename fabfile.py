927d6ca763158d8b4668e5a9e89f0fd91016daec
Stephen McDonald <steve@jupo.org>
2012-08-11 19:41:13 +1000

    Update fabfile to work with new-style tasks in Mezzanine.

--- fabfile.py
+++ fabfile.py
@@ -1,20 +1,19 @@
 
-from mezzanine.project_template.fabfile import *
+from fabric.api import cd, prefix, sudo, run, task
+from fabric.contrib.files import exists
+from mezzanine.project_template import fabfile as mezzfab
 
-
-_install = install
-_deploy = deploy
-_create = create
 
 doc_repos = ("mezzanine", "cartridge")
 
 
+@task
 def install():
-    _install()
-    with cd(env.venv_home):
+    mezzfab.install()
+    with cd(mezzfab.env.venv_home):
         if not exists("docs"):
             run("virtualenv docs --distribute")
-        with prefix("source %s/docs/bin/activate" % env.venv_home):
+        with prefix("source %s/docs/bin/activate" % mezzfab.env.venv_home):
             sudo("pip install sphinx cartridge fabric")
         for repo in doc_repos:
             repo_path = "docs/%s" % repo
@@ -23,17 +22,19 @@ def install():
                     run("hg clone http://bitbucket.org/stephenmcd/" + repo)
 
 
+@task
 def create():
-    _create()
+    mezzfab.create()
     manage("reset_demo")
 
 
+@task
 def deploy():
-    if not _deploy():
+    if not mezzfab.deploy():
         return
     for repo in doc_repos:
-        with cd("%s/docs/%s" % (env.venv_home, repo)):
+        with cd("%s/docs/%s" % (mezzfab.env.venv_home, repo)):
             run("hg pull")
             run("hg up -C")
-            with prefix("source %s/docs/bin/activate" % env.venv_home):
+            with prefix("source %s/docs/bin/activate" % mezzfab.env.venv_home):
                 run("sphinx-build docs docs/build")
