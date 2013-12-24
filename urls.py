78c21e2b6f37a50db2ef00cb2edfbc3eee508da5
Stephen McDonald <steve@jupo.org>
2013-12-24 20:42:29 +1100

    Upgrade to Mezzanine 3

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url, include
 from django.shortcuts import redirect, render
 
 from mezzanine.project_template.urls import urlpatterns
